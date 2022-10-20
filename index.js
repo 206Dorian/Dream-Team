176caa964cbce1db024f6ba885c88567215dc44c
Dorian Birch <206dorian@gmail.com>
2022-10-20 16:05:41 -0500

    added psedocode

---
+++ index.js
@@ -0,0 +1,38 @@
+// GIVEN a command-line application that accepts user input  **USING INQUIRER TO ACCEPT USER INPUT 
+// WHEN I am prompted for my team members and their information
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+// **USING ALL THAT DATA CREATE AN HTML FILE
+
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+// WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab  ***WILL OPEN EMAIL PROGRAM AND GITHUB WITH ANCHORS LIKE IN ACTIVITY 9***
+
+
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number   ***WHEN WE RUN NODE IN THE TERMINAL A PROMPT WILL APPEAR  = MANAGERS NAME, EMPLYEE ID, EMAIL ADDRESS AND OFFICE NUMBER.
+
+
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team  DIFFERENT FUNCTION WITH MENU OPTIONS ** ONCE THATS ADDED MAKE MENU LIKE THE LICENSE MENU.  ENGINEER, INTERN OR FINISH TEAM.
+//**IF/ELSE STAEMENT  */
+
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu **SEPREATE FINCTION FOR ENGINEER OPTION - INQUIRERE PROMPT ASKIGN FOR NAME, ID EMAIL, GITHIB USERNAME
+
+
+// WHEN I select the intern option
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu  **SEPERATE FUNCTION LIKE THE OTHERS
+// INQUIRERE PROMPT TO GET Named, ID , EMAIL, SCHOOL
+
+
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated  ** NEW FUNCTION WILL CREATE MY HTML FILE
+
+//** WILL BE AN ARRAY WITH OBJECTS  */  CREATE DIFFERENT SNIPPETS OF HTMLS  I.E. CARDS OR BOXES
+//**INSIDE SOURCE FOLDER IS WHERE ILL  BE RESPONSIBLE FOR CREATING THE CARDS, WILL NEED TO SPLITY UP ARRAY BASED ON THE TYPE OF EMPLOYEE
+
+//cREATE AN HTML CARD FOR EACH EMPLOYEE BASED ON THEIR TYPE, TAKE THOSE CARDS AND INJECT THEM INTO THE <BODY> OF A FINAL HTML TEMPLATE 
+// THAT TEMPLATE IS WHAT I'LL WRITE TO THE FINAL HTML FILE
+
+//CREATE CLASSES
